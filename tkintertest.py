dfd9925b3083a28a79e05a0bb0d186f9353afffd
Poolycode <79869512+Poolycode@users.noreply.github.com>
2021-05-11 10:48:02 -0700

    Add files via upload

--- tkintertest.py
+++ tkintertest.py
@@ -4,14 +4,9 @@ top = Tk()
 playList = []
 
 def results():
-    result = E1.get()
-    print(result)
-    print(type(result))
+   print(playlist)
 
-def addToList():
-    newItem = E1.get()
-    playList.append(newItem)
-    E1.delete(0, END)
+
 
 def exportList():
     with open('test.txt', 'w') as f:
@@ -30,16 +25,19 @@ def mainMenu():
     B1Main = Button(text = "Week 1", bg = "white", command = week1)
     B1Main.grid(column = 2, row = 2)
     
-    B2Main = Button(text = "Week 2", bg = "white")
+    B2Main = Button(text = "Week 2", bg = "white", command = week2)
     B2Main.grid(column = 2, row = 3)
     
     B3Main = Button(text = "Week 3", bg = "white")
     B3Main.grid(column = 2, row = 4)
 
 def week1():
-    clearWindow()
+    def addToList():
+        newItem = E1.get()
+        playList.append(newItem)
+        E1.delete(0, END)
     
-
+    clearWindow()
     #This is a Label widget
     L1 = Label(top, text = "Playlist Maker")
     L1.grid(column = 0, row = 1)
@@ -57,10 +55,31 @@ def week1():
 
     B3 = Button(text = "Export List", bg = "white", command = exportList)
     B3.grid(column = 0, row = 4)
-    """
-    Bclear = Buutton(text = " Clear Window ", bg = "white", command = clearWindow)
+    
+    Bclear = Buutton(text = " mainMenu ", bg = "white", command = mainMenu)
     Bclear.grid(colunm = 3, row = 1)
-    """
+
+def week2():
+    clearWindow()
+    L1W2 = Lable(top, text = "Dice Roller Program")
+    L1W2.grid(column = 0, row = 1)
+    
+    L2W2 = Lable(top, text = "How many sides?")
+    L2W2.grid(column = 0, row = 2)
+    
+    L3W2 = Lable(top, text = "How many rolls?")
+    L3W2.grid(column = 2, row = 2)
+    
+    E1W2 = Entry(top, bd = 5)
+    E1W2.grid(column = 0, row = 3)
+    
+    E2W2 = Entry(top, bd = 5)
+    E2W2.grid(column = 2, row = 3)
+    
+    B1W2 = Button(text = "Roll", bg = "Yellow")
+    B1W2.grid(column = 2, row = 4)
+
+    #to add: roll function and exit button
 
 if __name__ == "__main__":
     mainMenu()
